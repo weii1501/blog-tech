365671947b53f4cc018399dbdd9ad2b3e5c3f2e4
weii1501 <vodacluc15@gmail.com>
2024-01-31 10:35:12 +0700

    rm build next

--- next-src/build/static/chunks/webpack.js
+++ next-src/build/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "caabb519417abc42"; }
+/******/ 		__webpack_require__.h = function() { return "50409ee62200036e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
